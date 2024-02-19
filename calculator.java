693ad417548d5e414c754f5fecbfae4b3501418f
soumya <01fe22bca105@kletech.ac.in>
2024-02-19 15:05:10 +0530

    simple calculator

---
+++ calculator.java
@@ -0,0 +1,25 @@
+class calculator{
+    int num1=20;
+    int num2=10;
+
+    void add(){
+        int sum = num1+num2;
+        System.out.println("Addition of two numbers :"+sum);
+    }
+
+    void sub(){
+        int diff = num1-num2;
+        System.out.println("Difference of two numbers :"+diff);
+    }
+
+    void mul(){
+        int multi = num1*num2;
+        System.out.println("Multiplication of two numbers :"+multi);
+    }
+
+    void div(){
+        int divi = num1/num2;
+        System.out.println("Division of two numbers :"+divi);
+    }
+
+}
